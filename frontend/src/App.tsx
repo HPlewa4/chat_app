e678acc0921879cb2b9dc5f7cb2575e018bcf5ee
HPlewa4 <plewa.hubert4@gmail.com>
2026-03-13 23:29:21 +0100

    feat: prevent unlogged access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AllChats from './Components/AllChats';
 import ChatWindow from './Components/ChatWindow';
 import Login from './Components/Login';
@@ -15,14 +15,15 @@ function App() {
     const savedUser = localStorage.getItem('chat_user');
     return savedUser ? JSON.parse(savedUser) : null;
   });
+
   useEffect(() => {
-      if (currentUser) {
-        localStorage.setItem('chat_user', JSON.stringify(currentUser));
-      } else {
-        localStorage.removeItem('chat_user');
-      }
-    }, [currentUser]);
-    
+    if (currentUser) {
+      localStorage.setItem('chat_user', JSON.stringify(currentUser));
+    } else {
+      localStorage.removeItem('chat_user');
+    }
+  }, [currentUser]);
+
   return (
     <Router>
       <div className="App">
@@ -30,16 +31,26 @@ function App() {
           <Route 
             path="/" 
             element={
-              <div style={{ display: 'flex' }} className="App">
-                <AllChats currentUser={currentUser} setCurrentUser={setCurrentUser} />
-                <ChatWindow currentUser={currentUser} />
-              </div>
+              currentUser ? (
+                <div style={{ display: 'flex' }} className="App">
+                  <AllChats currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                  <ChatWindow currentUser={currentUser} />
+                </div>
+              ) : (
+                <Navigate to="/login" replace />
+              )
             } 
           />
 
           <Route 
             path="/login" 
-            element={<Login setCurrentUser={setCurrentUser} />} 
+            element={
+              !currentUser ? (
+                <Login setCurrentUser={setCurrentUser} />
+              ) : (
+                <Navigate to="/" replace />
+              )
+            } 
           />
         </Routes>
       </div>
